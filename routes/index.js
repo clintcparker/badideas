d1677488ec50e4437335532e8470340003c94755
Clint Parker <clintcparker@gmail.com>
2013-04-18 12:51:29 -0400

    added json support

--- routes/index.js
+++ routes/index.js
@@ -35,5 +35,9 @@ var dict2 = [
 exports.index = function(req, res){
     var co = dict1[Math.floor(Math.random()*dict1.length)];
     var group = dict2[Math.floor(Math.random()*dict2.length)];
+    if (req.is("json") || req.param("json")){
+        var badIdea = "It's like " + co + " for " + group + ".";
+        res.json({badidea: badIdea});
+    }
   res.render('index', { title: 'Bad Ideas', co:co, group:group});
 };

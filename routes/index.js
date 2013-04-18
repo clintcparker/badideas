7050fc14a5335c28342962ae29d0509710a2db40
Matthew Holden <matthew.holden@mindbodyonline.com>
2013-04-18 16:43:19 -0700

    Add Array.prototype.random for shiz and gigs

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,13 @@ var dict2 = [
     "aging rock'n'rollers"
 ];
 
+Array.prototype.random = function() {
+  return this[Math.floor(Math.random() * this.length)];
+}
+
 exports.index = function(req, res){
-    var co = dict1[Math.floor(Math.random()*dict1.length)];
-    var group = dict2[Math.floor(Math.random()*dict2.length)];
+    var co = dict1.random();
+    var group = dict2.random();
     if (req.is("json") || req.param("json")){
         var badIdea = "It's like " + co + " for " + group + ".";
         res.json({badidea: badIdea});
